434d69958cafea5a4ce022034b7916f880aaed10
helena-yarinovskaya <Alena.Yarynouskaya@itechart-group.com>
2019-03-04 23:35:03 +0300

    mobile header fix

--- src/components/header.js
+++ src/components/header.js
@@ -6,13 +6,15 @@ const Header = () => (
   <header className="header">
     <div className="header__container">
       <nav>
+        <div className="header__items">
         <Link className="header__item" to="#about-me">ABOUT ME</Link>
-        <Link className="header__item" to="#portfolio">PORTFOLIO</Link>
+        <Link className="header__item" to="#portfolio">PORTFOLIO</Link></div>   
         <Link to="/" className="header__logo">
           <Image file="logo.png" />
         </Link>
+        <div className="header__items">
         <Link className="header__item" to="#my-blog">MY BLOG</Link>
-        <Link className="header__item" to="#contact-me">CONTACT ME</Link>
+        <Link className="header__item" to="#contact-me">CONTACT ME</Link></div>
       </nav>
     </div>
   </header>
